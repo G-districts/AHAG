2d162b3c4dc7847d02f74ce3485a3ca6e1fc7e74
G-districts <admin@gdistrict.org>
2025-12-20 22:30:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ import json, os, time, sqlite3, traceback, uuid, re
 import time
 import secrets
 import string
-from datetime 
-import datetime, timezone
+from datetime import datetime, timezone
 from urllib.parse import urlparse
 from datetime import datetime
 from collections import defaultdict
